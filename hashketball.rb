e51fd54a40a108d4354ef1aa5433c56ebb2acf9d
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-26 14:33:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,7 +127,7 @@ def num_points_scored(player_name)
   all_players.flatten
   
   all_players.each do |player|
-  return player[points:] if player[player_name] = player_name
+  return player[points:] if player[player_name:] == player_name
   
 end
 
