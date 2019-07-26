e83519a2f587bdaf2164d35f47d2de0ea9df74d0
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-26 16:28:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -259,7 +259,8 @@ def winning_team
     def player_with_longest_name
       list = [] 
       game_hash.each do |location, team_data|
-        team_data[:players].each do 
+        team_data[:players].each do |name, stats|
+          
       
     end 
     
