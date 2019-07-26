cc2ab7cfcd28cdfd48b753d67776bb24a070f502
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-26 14:55:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -119,7 +119,7 @@ def game_hash
 end
 
 def num_points_scored(player_name)
-  game_hash.each do |location, team_date|
+  game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
       if data.include?(player_name)
         return game_hash[location][attribute][player][:points]
