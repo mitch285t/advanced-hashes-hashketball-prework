347219966df487c0077ee2bf0bc3dae609fa085c
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-26 16:40:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -276,7 +276,7 @@ def winning_team
         end 
       end 
       game_hash.each do |location, team_data|
-        team_data.each do |name, stats|
+        team_data[:players].each do |name, stats|
           if name == list_names.max_by(&:length) && most_steals.max
             return true 
           end 
