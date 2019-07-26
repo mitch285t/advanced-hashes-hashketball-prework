82f3106a26f4bfb0fc7b20148a0e3cd8b9ae91ca
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-26 15:49:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -201,7 +201,7 @@ def player_numbers(team_name)
     game_hash.collect do |location, team_data|
       team_data[:players].collect do |attribute, data|
         shoe_data.push(data[shoe])
-        biggest_shoe = shoe_data.sort(-1)
+        biggest_shoe = shoe_data.sort[-1]
     
     
     
