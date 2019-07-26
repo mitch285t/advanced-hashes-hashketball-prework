0f07fd6a1da97413a3d91c50a5856f366f1587f6
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-26 15:57:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -223,8 +223,8 @@ def player_numbers(team_name)
       most_points = points_data.sort(-1)
     end 
   end 
-  game_hash.collect do |location, data|
-    name = 
+  game_hash.collect do |location, team_data|
+    name = team_data
     
     
     
